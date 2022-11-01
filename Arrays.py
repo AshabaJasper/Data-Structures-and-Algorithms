dfaa91506a29d42d132303354bee0933cce927ea
Ashaba <ashabajasper@gmail.com>
2022-11-01 10:59:14 +0300

    Update

--- Arrays.py
+++ Arrays.py
@@ -85,10 +85,37 @@ for i in range(0, 3):
     print(b[i], end=" ")
 print()
 
-# adding an element using append()
+# adding an element using append() is only at the end
 b.append(4.4)
 
 print("Array after insertion : ", end=" ")
 for i in (b):
     print(i, end=" ")
 print()
+print("-----------------")
+# Removal of elements in a Array
+
+# importing "array" for array operations
+import array
+
+# initializing array with array values
+# initializes array with signed integers
+arr = array.array('i', [1, 2, 3, 1, 5])
+
+# printing original array
+print("The new created array is : ", end="")
+for i in range(0, 5):
+    print(arr[i], end=" ")
+
+print("\r")
+
+# using pop() to remove element at 2nd position
+print("The popped element is : ", end="")
+print(arr.pop(2))
+
+# printing array after popping
+print("The array after popping is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+
+print("\r")
