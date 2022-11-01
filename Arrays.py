e4fe8894b39a865113534060a38bea5f49a5287f
Ashaba <ashabajasper@gmail.com>
2022-11-01 09:25:46 +0300

    Update

--- Arrays.py
+++ Arrays.py
@@ -1,2 +1,13 @@
 #Arrays are containers which are able to store more than one item
 #at the same time
+
+#The difference between Arrays and Lists in python is arrays can only store one data type
+#How to use arrays
+
+#using the array module
+
+#method one:
+import array
+
+#create and array with
+array.array("Hello")
