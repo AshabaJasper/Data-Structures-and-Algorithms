8bfae8d65e13e9b9b0d9f7f7028dc34df4720cd6
Ashaba <ashabajasper@gmail.com>
2022-11-01 09:36:11 +0300

    Update

--- Arrays.py
+++ Arrays.py
@@ -10,4 +10,11 @@
 import array
 
 #create and array with
-array.array("Hello")
+a = array.array('i', [1, 2, 3])
+
+# printing original array
+print("The new created array is : ")
+for i in range(0, 3):
+    print(a[i], end=",")
+print()
+
